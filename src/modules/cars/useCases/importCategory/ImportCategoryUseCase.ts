28245fbafb31f6055b8857ddd4979339ed0bfa4c
Matheus-nb <matheusalg.nb@gmail.com>
2021-09-02 20:28:12 -0300

    Swagger Documentation

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,16 +1,59 @@
 import csvParse from 'csv-parse';
 import fs from 'fs';
 
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
   // eslint-disable-next-line no-undef
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
-    const parseFile = csvParse();
+  loadCategory(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const parseFile = csvParse();
 
-    stream.pipe(parseFile);
+      const categories: IImportCategory[] = [];
 
-    parseFile.on('data', async line => {
-      console.log(line);
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', async line => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on('error', err => {
+          reject(err);
+        });
+    });
+  }
+
+  // eslint-disable-next-line no-undef
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategory(file);
+
+    categories.map(async category => {
+      const { name, description } = category;
+
+      const existCategory = this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
     });
   }
 }
